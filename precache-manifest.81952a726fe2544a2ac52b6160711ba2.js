f12734ea9ccf58569f4e225b1a7c849a4e44441d
saravrsaljko <sara.vrsaljko@gmail.com>
2020-05-18 14:11:59 +0200

    Updates

---
+++ precache-manifest.81952a726fe2544a2ac52b6160711ba2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b5ebe8cef5f49004c5686535abfa0e",
+    "url": "/novi-react/index.html"
+  },
+  {
+    "revision": "5100c32e91c98cfc5c54",
+    "url": "/novi-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19a743a163bc217f807b",
+    "url": "/novi-react/static/js/2.ab99bac9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/novi-react/static/js/2.ab99bac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5100c32e91c98cfc5c54",
+    "url": "/novi-react/static/js/main.882fcd9f.chunk.js"
+  },
+  {
+    "revision": "32b86730187c4a470b78",
+    "url": "/novi-react/static/js/runtime-main.15a9b549.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/novi-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
